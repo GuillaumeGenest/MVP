e1180045a5d779dd262c0596e15997de1f00ec0a
Guillaume <guillaume-genest2@hotmail.fr>
2023-10-13 10:38:57 +0200

    Initial Commit

---
+++ MVP/MVPApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVPApp.swift
+//  MVP
+//
+//  Created by Guillaume Genest on 13/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct MVPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
